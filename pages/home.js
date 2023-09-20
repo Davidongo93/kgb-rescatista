66a5dbcdf2ca267c8c097dd4b08fc846791d0972
David Orlando Miranda <domirandar@unal.edu.co>
2023-09-20 16:23:06 -0500

    fix (link moved to view1)

--- pages/home.js
+++ pages/home.js
@@ -1,8 +1,6 @@
-// pages/home.js (Home)
 import Navbar from '../components/navBar/NavBar';
 import View1 from '../components/view1';
 import Footer from '../components/footer/footer';
-import Link from 'next/link';
 
 export default function Home() {
   return (
@@ -10,9 +8,6 @@ export default function Home() {
       <Navbar />
       <View1 />
       <Footer />
-      <Link href="/">
-        Go to Landing
-      </Link>
     </div>
   );
 }
