9d06c15bc9cdb8b9bbd596529fb034a3d0da7701
David Orlando Miranda <domirandar@unal.edu.co>
2023-09-20 15:14:43 -0500

    Feat (client/Landing video, container)

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,36 @@
-import Link from 'next/link';
-import Image from 'next/image';
+import { useRouter } from 'next/router';
+import RootLayout from '@/app/layout';
 
 export default function Landing() {
+  const router = useRouter();
+
+  const goToHome = () => {
+    router.push('/home');
+  };
+
   return (
-    <div className="min-h-screen relative">
-            <Image
-            src="/landing.jpeg"
-            alt="Next.js Logo"
-            layout="fill"
-            objectFit="cover"
-          />
-      <div
-        className="absolute inset-0 w-full h-full bg-cover bg-center"
-        style={{ backgroundImage: 'url("/landing.jpeg")' }}
-      ></div>
-      
-      {/* Contenido superpuesto */}
-      <div className="absolute inset-0 flex flex-col justify-center items-center bg-opacity-75 bg-white">
-        <h1 className="text-4xl font-bold mb-6">Landing Page</h1>
-        <Link 
-          href="/home"
-          className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition"
-        >
-          home
-        </Link>
+    <RootLayout>
+      <div className="min-h-screen relative overflow-hidden">
+        {/* Video de fondo */}
+        <video autoPlay muted loop className="absolute inset-0 object-cover w-full h-full">
+          <source src="/landingAnimation.mp4" type="video/mp4" />
+          Tu navegador no soporta el elemento de video.
+        </video>
+
+        {/* Contenido superpuesto */}
+        <div className="absolute inset-0 flex flex-col items-center justify-center text-white text-center">
+          <div>
+            <div className="text-3xl font-bold">Kevin Alexander Galeano Barbosa</div>
+            <div className="text-xl">Bombero - Rescatista - Instructor</div>
+            <button
+              onClick={goToHome}
+              className="bg-orange-500 text-white px-4 py-2 rounded hover:bg-orange-600 transition mt-4 text-center"
+            >
+              Home
+            </button>
+          </div>
+        </div>
       </div>
-    </div>
+    </RootLayout>
   );
 }
