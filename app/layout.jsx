db803fb1e0317a3126375ee8b877ce89f5c8a781
David Orlando Miranda <domirandar@unal.edu.co>
2023-10-03 20:21:11 -0500

    layout clear

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,32 +1,17 @@
 import './globals.css'
-import { useRouter } from 'next/router';
 import { Inter } from 'next/font/google';
-import Landing from '../pages/landing';
-import Home from '@/pages/home';
+import Social from '@/components/social/Social';
 
 const inter = Inter({ subsets: ['latin'] });
-
 // export const metadata = {
 //   title: 'Kevin Galeano - Rescatista',
 //   description: 'Firefighter - Trainer - Rescuer',
 // };
-
 export default function RootLayout({ children }) {
-  const router = useRouter();
 
-  // Verifica la ruta actual para renderizar condicionalmente
-  if (router.pathname === '/') {
     return (
       <div>
-        <Landing />
-      </div>
-    );
-  } else if (router.pathname === '/home') {
-    return (
- <Home></Home>
-    );
-  }
-
-  // Si ninguna condición se cumple, muestra solo el contenido
-  return <div>{children}</div>;
+        {children}
+     </div>
+    )
 }
