6a4e6a5a128d66b64a1798f8f0e8e03d14691509
David Orlando Miranda <domirandar@unal.edu.co>
2023-10-04 10:38:01 -0500

    a label replaces next/navigation issue

--- app/page.js
+++ app/page.js
@@ -1,13 +1,6 @@
-"use client"
-// import Social from '@/components/social/Social.jsx';
-import { useRouter } from 'next/navigation';
+import Social from '@/components/social/Social.jsx';
 export default function Landing() {
-  const router = useRouter();
-
-  const goToHome = () => {
-    router.push('/home');
-  };
-
+  
   return (
       <div className="min-h-screen relative overflow-hidden">
         {/* Video de fondo */}
@@ -22,13 +15,16 @@ export default function Landing() {
             <div className="text-3xl font-bold">Kevin Alexander Galeano Barbosa</div>
             <div className="text-xl">Bombero - Rescatista - Instructor</div>
             <button
-              onClick={goToHome}
               className="bg-orange-500 text-white px-4 py-2 rounded hover:bg-orange-600 transition mt-4 text-center"
             >
-              <code className="text-white-400">Explore</code>
+              <code className="text-white-400">
+              <a
+              href="/home"
+              >Explore</a></code>
             </button>
           </div>
         </div>
+        <Social/>
       </div>
   );
 }
