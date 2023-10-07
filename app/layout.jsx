65ff618f7ab4c29d351ff0427fac897f88c90f4c
David Orlando Miranda <domirandar@unal.edu.co>
2023-10-06 21:18:16 -0500

    green

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,13 +1,13 @@
 import './globals.css'
 import { Inter } from 'next/font/google';
-
+import 
 
 const inter = Inter({ subsets: ['latin'] });
 
-export const metadata = {
-  title: 'Kevin Galeano - Rescatista',
-  description: 'Firefighter - Trainer - Rescuer',
-};
+// export const metadata = {
+//   title: 'Kevin Galeano - Rescatista',
+//   description: 'Firefighter - Trainer - Rescuer',
+// };
 export default function RootLayout({ children }) {
 
     return (
