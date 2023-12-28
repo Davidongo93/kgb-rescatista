93d9cb9d0033fc067523551af58c9be3b5fde3e6
David Orlando Miranda Roa <domirandar@unal.edu.co>
2023-12-27 21:35:52 -0500

    layout fit

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,16 +1,8 @@
-// RootLayout.js
-
-import { useRouter } from "next/navigation";
-import ChildComponent from "./ChildComponent";
 
 export default function RootLayout({ children }) {
-  const router = useRouter();
-
   return (
-    <>
-      {/* Pasa el router como prop al componente hijo */}
-      <ChildComponent router={router} />
-      {children}
+  <>
+      { children }
     </>
   );
 }
